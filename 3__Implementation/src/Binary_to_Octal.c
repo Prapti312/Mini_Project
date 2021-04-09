f66759e5301f80d294ef0fcec384377cbd75e366
PRAPTI BHAJIYAWALA <praptibhajiyawala312@gmail.com>
2021-04-09 10:36:48 +0530

    Added Source Code

---
+++ 3__Implementation/src/Binary_to_Octal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+#include "conversion.h"
+long int Binary_to_Octal(long int bin)
+{
+    int x=0,remainder,sum=0,remaining[100],length=0;
+
+    while(bin!=0)
+    {
+        remainder=bin%10;
+        bin=bin/10;
+        sum=sum+remainder*pow(2,x);
+        x++;
+    }
+    x=0;
+    while(sum!=0)
+    {
+        remaining[x]=sum%8;
+        sum=sum/8;
+        x++;
+        length++;
+    }
+
+    long long int i;
+
+    long long int answer=remaining[length-1];
+
+    for(i=length-2; i>=0 ; i--){
+    	answer=(answer*10) + remaining[i];
+	}
+    return answer;
+}
---
+++ 3__Implementation/src/Decimal_to_Binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+#include "conversion.h"
+#include<stdlib.h>
+
+
+long int Decimal_to_Binary(long int dec)
+{
+    int remainder[50],x,length=0;
+    do
+    {
+        remainder[x]=dec%2;
+        dec=dec/2;
+        x++;
+        length++;
+    }
+    while(dec!=0);
+
+    long long int i;
+
+    long long int answer=remainder[length-1];
+
+    for(i=length-2; i>=0 ; i--){
+    	answer=(answer*10) + remainder[i];
+	}
+    return answer;
+}
